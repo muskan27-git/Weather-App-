56576d9baba0062d6da73132f6180f2d0604a169
Muskan <muskankasera1999@gmail.com>
2021-08-06 22:51:33 +0530

    first commit

---
+++ Hello/home/views.py
@@ -0,0 +1,44 @@
+from django.http import request
+from django.shortcuts import render , HttpResponse
+import urllib.request
+import json
+
+# Create your views here.
+
+# def index(request):
+#     context={
+#         "variable1":"My name is Muskan",
+#     }
+#     return render(request,'index.html',context)
+
+
+
+
+
+def index(request):
+
+    if request.method == 'POST':
+        city = request.POST['city']
+
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
+                                        city + '&units=metric&appid=f92612411a675bee446b75a962294409').read()
+        list_of_data = json.loads(source)
+
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ', '
+            + str(list_of_data['coord']['lat']),
+
+            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
+        }
+        print(data)
+    else:
+        data = {}
+
+    return render(request, 'index1.html', data)
+
